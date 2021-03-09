e2a4fbc06d008e3bc7dfa7077bfa3115732f08bf
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-08 22:18:24 -0500

    Update cutie.js

--- cutie.js
+++ cutie.js
@@ -1,25 +1,39 @@
 
-const emote=['  kaestr1Cool    ',
-              '  kaestr1Hug     ',
-              '  maizCool       ',
-              '  abbybaCool     ',
-              '  smolCool       ',
-              '  lycelCool      ',
-              '  lycelYes       ',
-              '  therea1896Cool ',
-              '  smolCheese     ',
-              '  abbybaHug      ',
-              '  elizab34Uwu    ',
-              '  pcrowHug       '];
+const happy=['  kaestr1Cool    ',
+             '  kaestr1Hug     ',
+             '  maizCool       ',
+             '  abbybaCool     ',
+             '  smolCool       ',
+             '  lycelCool      ',
+             '  lycelYes       ',
+             '  therea1896Cool ',
+             '  smolCheese     ',
+             '  abbybaHug      ',
+             '  elizab34Uwu    ',
+             '  pcrowHug       '];
+
+const cry=[' kaestr1Cry    ',
+           ' elizab34Hands ',
+           ' lycelHands    ',
+           ' smolHands     ',
+           ' abbybaCry     ',
+           ' archit3Cry    ',
+           ' pcrowCry      ',
+           ' smolCry       ']
 
 start=" Did y'all know ";
 middle=" is a kecking ";
 trait=" cutie ";
-end=" I bed ya didn't "+emote[Math.floor(Math.random()*emote.length)];
+end=" I bet ya didn't "+emote[Math.floor(Math.random()*emote.length)];
+
+
+if(/Kae/i.test(query)){
+  trait=" cute lil' flower? maizBlush ";
+}
 
 
 if(query.length==0) {
-  o+='no one';
+  query+=' no one ';
 }
 
 else {
@@ -27,15 +41,4 @@ else {
 }
 
 
-o+=" is a hecking "; 
-
-if(/Kae/i.test(query)){
-  trait=" cute lil' flower? maizBlush ";
-}
-
-else{
-  o+='cutie??';
-}
-
-o+=end;
-o;
+start+query+middle+trait+end;
