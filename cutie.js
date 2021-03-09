185ac59fc935b767c4a6cf1192ade2b309013912
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-08 21:39:58 -0500

    Create cutie.js

---
+++ cutie.js
@@ -0,0 +1,22 @@
+
+const cheese=['  kaestr1Cool    ',
+              '  kaestr1Hug     ',
+              '  maizCool       ',
+              '  abbybaCool     ',
+              '  smolCool       ',
+              '  lycelCool      ',
+              '  lycelYes       ',
+              '  therea1896Cool ',
+              '  smolCheese     ',
+              '  abbybaHug      ',
+              '  elizab34Uwu    ',
+              '  pcrowHug       '];
+
+o="Did y'all know ";
+end=" smolHands whyyy??";
+if(query.length==0){o+='no one';}
+else{	o+=query;	end=' I bet ya didnt! '+chz[Math.floor(Math.random()*chz.length)];}
+o+=" is a hecking "; 
+if(/Kae/i.test(query)){	o+='cute lil flower maizBlush ??';}
+else{	o+='cutie??';}
+o+=end;o;
