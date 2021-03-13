0b3442378c9ff4ed9d327fb1b045633873a5cd92
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-12 21:54:02 -0500

    Update mcfacts.js

--- mcfacts.js
+++ mcfacts.js
@@ -2,12 +2,12 @@ const facts=[" test1 ",
              " test2 "];
 
 start=" Did you know ";
-middle=facts[Math.random()facts.length];
+middle=facts[Math.floor(Math.random()facts.length)];
 
 final=start+middle;
 
 if (/^version$|^v$/i.test(query)) {
-    final=" Last updated march 12 2021 at 21:49 EST. "
+    final=" Last updated march 12 2021 at 21:49 EST. ";
 }
 
-final;
+final
