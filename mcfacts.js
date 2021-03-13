ece00fb2e8da9505cbbf474d66afec9d01422035
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-12 22:05:26 -0500

    Update mcfacts.js

--- mcfacts.js
+++ mcfacts.js
@@ -1,13 +1,30 @@
-const facts=[" test1 ",
-             " test2 "];
+const facts=[" Minecraft is a sandbox game?? ",
+             " Minecraft is a sandbox game?? "];
+
+const omg=[' smolWow     ',
+           ' pcrowWow    ',
+           ' smolD       ',
+           ' bryce4Eyes  ',
+           ' lycelW      ',
+           ' lycelAAA    ',
+           ' smolBrain   ',
+           ' pcrowBrain  ',
+           ' cjyaPog     ',
+           ' kaestr1Eyes ',
+           ' maizEyes    ',
+           ' maizBlush   ',
+           ' bbytesWut   ',
+           ' abibeaLurk  ',
+           ' elizab34Pog '];
 
 start=" Did you know ";
-middle=facts[Math.floor(Math.random()*facts.length)];
+fact=facts[Math.floor(Math.random()*facts.length)];
+emote=omg[Math.floor(Math.random()*omg.length)];
 
-final=start+middle;
+final=start+middle+emote;
 
 if (/^version$|^v$/i.test(query)) {
-    final=" Last updated March 12 2021 at 22:01 EST. ";
+    final=" Last updated March 12 2021 at 22:05 EST. ";
 }
 
 final
