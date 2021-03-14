fa54fb63aa6914d0d601b080663a3f8c8b20c209
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-14 18:56:15 -0500

    Update and rename botwfacts.js to zeldafacts.js

---
+++ zeldafacts.js
@@ -0,0 +1,70 @@
+/*BOTW facts retrieved from 
+https://www.thegamer.com/legend-zelda-breath-of-the-wild-things-didnt-know/ 
+https://www.thegamer.com/legend-of-zelda-hilarious-reasons-economy-nonsense/ 
+*/
+
+const facts=[" BOTW is the first Zelda game without composer Koji Kondo??                     ",
+             " BOTW's Zelda's voice actor was not actually British??                          ",
+             " BOTW was Originally Just For The Wii U??                                       ",
+             " in BOTW, Satori Mountain Is Named After Late Nintendo President??              ",
+             " in BOTW, the Great Plateau Was The First Area Built For The Game??             ",
+             " in BOTW, The Game Started With A Small Team??                                  ",
+             " BOTW's Guardians Are Based On The Jomon Period Of Japanese History??           ",
+             " in BOTW, Zelda's Character Was The Most Difficult To Design??                  ",
+             " Breath Of The Wild’s Hyrule Is About 12 Times As Large As Twilight Princess'?? ",
+             " BOTW Is The Best-Selling Zelda Game Of All Time??                              ",
+             " The Size Of Rupees Is Ridiculous in Zelda games??                              ",
+             " Money Is An Endless Resource in Zelda games??                                  ",
+             " in some games, Robbery Is Part Of The legend of Zelda's Economy??              ",
+             " you gotta pay Money To Hold More Money in some Zelda games??                   ",
+             " Prices are really weird in Zelda games??                                       "];
+
+const didntknow=[" Well I didn't!!                ",
+                 " HUHH?? I DIDN'T KNOW THAT!!    ",
+                 " REALLY??                       ",
+                 " You're being serious, right??  ",
+                 " Yes?? AND YOU DIDN'T TELL ME?? ",
+                 " OMG YOU SHOULD'VE TOLD ME!!    ",
+                 " I am like.. speechless..       ",
+                 " I had no idea!                 ",
+                 " NO WAY!!                       "];
+
+const omg=[' smolWow     ',
+           ' pcrowWow    ',
+           ' smolD       ',
+           ' bryce4Eyes  ',
+           ' lycelW      ',
+           ' lycelAAA    ',
+           ' smolBrain   ',
+           ' pcrowBrain  ',
+           ' cjyaPog     ',
+           ' kaestr1Eyes ',
+           ' maizEyes    ',
+           ' maizBlush   ',
+           ' abibeaLurk  ',
+           ' elizab34Pog '];
+
+start=" Did you know ";
+fact=facts[Math.floor(Math.random()*facts.length)];
+emote=omg[Math.floor(Math.random()*omg.length)];
+
+end="  ";
+
+
+if (Math.random() <= 0.5) {
+    end=didntknow[Math.floor(Math.random()*didntknow.length)]+omg[Math.floor(Math.random()*omg.length)];
+}
+
+
+final=start+fact+emote+end;
+
+if (/^facts$|^v$/i.test(query)) {
+    final=" Facts retrieved from the following websites: https://www.thegamer.com/legend-zelda-breath-of-the-wild-things-didnt-know/ https://www.thegamer.com/legend-of-zelda-hilarious-reasons-economy-nonsense/  ";
+}
+
+if (/^version$|^v$/i.test(query)) {
+    final=" Added 15 new Zelda facts. - March 14 2021 at 18:56 UTC-5. ";
+}
+
+
+final
