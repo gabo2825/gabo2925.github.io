9f82e1894b913e3de1cb83aeb746ec608c7d9e83
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-27 18:45:01 -0500

    Update abipets.js

--- abipets.js
+++ abipets.js
@@ -1,9 +1,14 @@
-if (/ronon/i.test(pet)) {
-    " You used the Ronon command. ";
+if (/^v$|^version$/i.test(query)) {
+    " Ronon (the red dog) is a cavalier King Charles Spaniel. He doesn’t care about anything except for food and naps. ";
 }
-else if (/teyla/i.test(pet)) {
-    " You used the Teyla command. ";
-}
-else if (/pippin/i.test(pet)) {
-    " You used the Pippin command. ";
+else {
+    if (/ronon/i.test(pet)) {
+        " Ronon (the red dog) is a cavalier King Charles Spaniel. He doesn’t care about anything except for food and naps. ";
+    }
+    else if (/teyla/i.test(pet)) {
+        " Teyla (the white dog) is a cavalier King Charles Spaniel. She once found a toy in a cabinet 5’ above her head just by smell alone. ";
+    }
+    else if (/pippin/i.test(pet)) {
+        " Pippin is a domestic short hair cat. Despite being free fed and given wet food twice a day he wants you to know he’s still starving. ";
+    }
 }
