9c6a4384bf29dcef9edcfe8ef2b633bf6091fef8
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-27 19:36:29 -0500

    Update abipets.js

--- abipets.js
+++ abipets.js
@@ -1,22 +1,34 @@
 if (/^version$|^v$/i.test(query)) {
-    " Made the change from !ronon, !teyla, !pippin to !pets. - March 27 2021 at 19:27 UTC-5. ";
+    " Made a while command and I hope this doesn't break. - March 27 2021 at 19:36 UTC-5. ";
 }
 else {
-    if (/^ronon$/i.test(query)) {
-        facts=[" He doesn’t care about anything except for food and naps. ",
-               " His name comes from the SciFi series: Stargate Atlantis! ",
-               " He broke his leg being 6 months old falling off from a bed. kaestr1Cry "];
-        " Ronon (the red dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
-    }
-    else if (/^teyla$/i.test(query)) {
-        facts=[" She once found a toy in a cabinet 5’ above her head just by smell alone. ",
-               " She loves sneezing on people's faces. abibeaHug ",
-               " Her name comes from the SciFi series: Stargate Atlantis! "];
-        " Teyla (the white dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
-    }
-    else if (/^pippin$/i.test(query)) {
-        facts=[" Despite being free fed and given wet food twice a day he wants you to know he’s still starving. ",
-               " He once activated sub only mode in Kae's chat. pcrowGiggle "];
-        " Pippin is a domestic short hair cat. "+facts[Math.floor(Math.random()*facts.length)];
-    }
+    count=1
+    while (count>0) {
+        if (/^ronon$/i.test(query)) {
+            facts=[" He doesn’t care about anything except for food and naps. ",
+                   " His name comes from the SciFi series: Stargate Atlantis! ",
+                   " He broke his leg being 6 months old falling off from a bed. kaestr1Cry "];
+            " Ronon (the red dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
+            count=count-1;
+        }
+        else if (/^teyla$/i.test(query)) {
+            facts=[" She once found a toy in a cabinet 5’ above her head just by smell alone. ",
+                   " She loves sneezing on people's faces. abibeaHug ",
+                   " Her name comes from the SciFi series: Stargate Atlantis! "];
+            " Teyla (the white dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
+            count=count-1;
+        }
+        else if (/^pippin$/i.test(query)) {
+            facts=[" Despite being free fed and given wet food twice a day he wants you to know he’s still starving. ",
+                   " He once activated sub only mode in Kae's chat. pcrowGiggle "];
+            " Pippin is a domestic short hair cat. "+facts[Math.floor(Math.random()*facts.length)];
+            count=count-1;
+        }
+        else {
+            query=["ronon",
+                   "pippin",
+                   "teyla"];
+            query[Math.floor(Math.random()*query.length)];
+        }
+    }   
 }
