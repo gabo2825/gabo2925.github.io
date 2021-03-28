4a11c08e7ee6a5cccf6aa5aa64856e677f004dcf
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-27 20:22:56 -0500

    Update abipets.js

--- abipets.js
+++ abipets.js
@@ -1,5 +1,5 @@
 if (/^version$|^v$/i.test(query)) {
-    " Added a randomizer when no query is provided. - March 27 2021 at 20:18 UTC-5. ";
+    " Fixed sth about the randomizer, also changed 'sub' to 'emote'. - March 27 2021 at 20:22 UTC-5. ";
 }
 else {
     count=0;
@@ -9,28 +9,39 @@ else {
             if (count>=1) {
                 intro=" Ronon (the red dog) is a ";
             }
+            else {
+                count++
+            }
             facts=[" He doesn’t care about anything except for food and naps. ",
                    " His name comes from the SciFi series: Stargate Atlantis! ",
                    " He broke his leg being 6 months old falling off from a bed. kaestr1Cry "];
             intro+" cavalier King Charles Spaniel: "+facts[Math.floor(Math.random()*facts.length)];
         }
+        
         else if (/^teyla$/i.test(query)) {
             count++;
             if (count>=1) {
                 intro=" Teyla (the white dog) is a ";
+            }
+            else {
+                count++
             }
             facts=[" She once found a toy in a cabinet 5’ above her head just by smell alone. ",
                    " She loves sneezing on people's faces. abibeaHug ",
                    " Her name comes from the SciFi series: Stargate Atlantis! "];
             intro+" Teyla (the white dog) is a cavalier King Charles Spaniel: "+facts[Math.floor(Math.random()*facts.length)];
         }
+        
         else if (/^pippin$/i.test(query)) {
             count++;
             if (count>=1) {
                 intro=" Pippin is a ";
             }
+            else {
+                count++
+            }
             facts=[" Despite being free fed and given wet food twice a day he wants you to know he’s still starving. ",
-                   " He once activated sub only mode in Kae's chat. pcrowGiggle "];
+                   " He once activated emote only mode in Kae's chat. pcrowGiggle "];
             intro+" domestic short hair cat: "+facts[Math.floor(Math.random()*facts.length)];
         }
         else {
