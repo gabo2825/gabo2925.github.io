f8de741b1f6979587768b653e1f17519067967eb
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-27 19:18:34 -0500

    Update abipets.js

--- abipets.js
+++ abipets.js
@@ -1,12 +1,17 @@
 if (/^version$|^v$/i.test(query)) {
-    " Gabo's dumb we'll take forever to figure this out pepeDS - March 27 2021 at 19:08 UTC-5. ";
+    " Added new messages! - March 27 2021 at 19:18 UTC-5. ";
 }
 else {
     if (/ronon/i.test(pet)) {
-        " Ronon (the red dog) is a cavalier King Charles Spaniel. He doesn’t care about anything except for food and naps. ";
+        facts=[" He doesn’t care about anything except for food and naps. ",
+               " His name comes from the SciFi series: Stargate Atlantis "];
+        " Ronon (the red dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
     }
     else if (/teyla/i.test(pet)) {
-        " Teyla (the white dog) is a cavalier King Charles Spaniel. She once found a toy in a cabinet 5’ above her head just by smell alone. ";
+        facts=[" She once found a toy in a cabinet 5’ above her head just by smell alone. ",
+               " She loves sneezing on people's faces. abibeaHug ",
+               " Her name comes from the SciFi series: Stargate Atlantis "];
+        " Teyla (the white dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
     }
     else if (/pippin/i.test(pet)) {
         facts=[" Despite being free fed and given wet food twice a day he wants you to know he’s still starving. ",
