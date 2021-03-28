c8d3068421ebb6bdf9ef215d6173e8184e82b780
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-27 19:02:34 -0500

    Update abipets.js

--- abipets.js
+++ abipets.js
@@ -1,5 +1,5 @@
 if (/^version$|^v$/i.test(query)) {
-    " I had to reupdate this YEP - March 27 2021 at 18:55 UTC-5. ";
+    " I FORGOT A FREAKING SEMICOLON YEP - March 27 2021 at 18:55 UTC-5. ";
 }
 else {
     if (/ronon/i.test(pet)) {
