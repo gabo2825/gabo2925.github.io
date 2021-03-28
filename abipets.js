a3ef7cc3bf9a3248557c5c1f82305039a1a745d9
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-27 19:46:53 -0500

    Update abipets.js

--- abipets.js
+++ abipets.js
@@ -1,25 +1,25 @@
 if (/^version$|^v$/i.test(query)) {
-    " i'm trying so hard - March 27 2021 at 19:44 UTC-5. ";
+    " i'm trying so hard I FORGOT MORE SEMICOLONS - March 27 2021 at 19:46 UTC-5. ";
 }
 else {
-    count=1;
-    while (count>0) {
+    count=0;
+    while (count=0) {
         if (/^ronon$/i.test(query)) {
-            count=count-1
+            count++;
             facts=[" He doesn’t care about anything except for food and naps. ",
                    " His name comes from the SciFi series: Stargate Atlantis! ",
                    " He broke his leg being 6 months old falling off from a bed. kaestr1Cry "];
             " Ronon (the red dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
         }
         else if (/^teyla$/i.test(query)) {
-            count=count-1
+            count++;
             facts=[" She once found a toy in a cabinet 5’ above her head just by smell alone. ",
                    " She loves sneezing on people's faces. abibeaHug ",
                    " Her name comes from the SciFi series: Stargate Atlantis! "];
             " Teyla (the white dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
         }
         else if (/^pippin$/i.test(query)) {
-            count=count-1
+            count++;
             facts=[" Despite being free fed and given wet food twice a day he wants you to know he’s still starving. ",
                    " He once activated sub only mode in Kae's chat. pcrowGiggle "];
             " Pippin is a domestic short hair cat. "+facts[Math.floor(Math.random()*facts.length)];
