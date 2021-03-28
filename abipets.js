28337ab68c5d2aa9dd92e0d8e2a57ef6f48cccf4
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-27 20:18:46 -0500

    Update abipets.js

--- abipets.js
+++ abipets.js
@@ -1,34 +1,45 @@
 if (/^version$|^v$/i.test(query)) {
-    " i may have fixed it - March 27 2021 at 20:00 UTC-5. ";
+    " Added a randomizer when no query is provided. - March 27 2021 at 20:18 UTC-5. ";
 }
 else {
     count=0;
     while (count<1) {
         if (/^ronon$/i.test(query)) {
             count++;
+            if (count>=1) {
+                intro=" Ronon (the red dog) is a ";
+            }
             facts=[" He doesn’t care about anything except for food and naps. ",
                    " His name comes from the SciFi series: Stargate Atlantis! ",
                    " He broke his leg being 6 months old falling off from a bed. kaestr1Cry "];
-            " Ronon (the red dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
+            intro+" cavalier King Charles Spaniel: "+facts[Math.floor(Math.random()*facts.length)];
         }
         else if (/^teyla$/i.test(query)) {
             count++;
+            if (count>=1) {
+                intro=" Teyla (the white dog) is a ";
+            }
             facts=[" She once found a toy in a cabinet 5’ above her head just by smell alone. ",
                    " She loves sneezing on people's faces. abibeaHug ",
                    " Her name comes from the SciFi series: Stargate Atlantis! "];
-            " Teyla (the white dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
+            intro+" Teyla (the white dog) is a cavalier King Charles Spaniel: "+facts[Math.floor(Math.random()*facts.length)];
         }
         else if (/^pippin$/i.test(query)) {
             count++;
+            if (count>=1) {
+                intro=" Pippin is a ";
+            }
             facts=[" Despite being free fed and given wet food twice a day he wants you to know he’s still starving. ",
                    " He once activated sub only mode in Kae's chat. pcrowGiggle "];
-            " Pippin is a domestic short hair cat. "+facts[Math.floor(Math.random()*facts.length)];
+            intro+" domestic short hair cat: "+facts[Math.floor(Math.random()*facts.length)];
         }
         else {
-        query=["ronon",
-               "pippin",
-               "teyla"];
-        query=query[Math.floor(Math.random()*query.length)];
+            query=["Ronon",
+                   "Pippin",
+                   "Teyla"];
+            query=query[Math.floor(Math.random()*query.length)];
+            intro=" Here's a fact about "+query+", Abi's ";
+            count--;
         }
     }   
 }
