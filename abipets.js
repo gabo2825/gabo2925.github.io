16820511586c514bcb2e0e4a8e03fb80cd1f40ed
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-03-27 19:23:35 -0500

    Update abipets.js

--- abipets.js
+++ abipets.js
@@ -1,10 +1,11 @@
 if (/^version$|^v$/i.test(query)) {
-    " Corrected some gramatical errors. - March 27 2021 at 19:18 UTC-5. ";
+    " Added a new message!. - March 27 2021 at 19:23 UTC-5. ";
 }
 else {
     if (/ronon/i.test(pet)) {
         facts=[" He doesn’t care about anything except for food and naps. ",
-               " His name comes from the SciFi series: Stargate Atlantis! "];
+               " His name comes from the SciFi series: Stargate Atlantis! ",
+               " He broke his leg being 6 months old falling off from a bed. kaestr1Cry "];
         " Ronon (the red dog) is a cavalier King Charles Spaniel. "+facts[Math.floor(Math.random()*facts.length)];
     }
     else if (/teyla/i.test(pet)) {
