d42331c2910cda7e1ac76ff57895b8143e0774c6
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-04-07 22:07:28 -0500

    Create rps.js

---
+++ rps.js
@@ -0,0 +1,30 @@
+const nbprob=["rock","paper","scissors"];
+nb=nbprob[Math.floor(Math.random()*nbprob.length)];
+nbt="I chose "+nb;
+
+if(query.length==0) {'Please specify "r", "p", "s".';}
+else{
+if(/^rock$|^r$/i.test(query)){
+query="rock";
+if(/^scissors$/i.test(nb)){
+res=1
+}
+}
+else if(/^paper$|^p$/i.test(query)){
+query="paper";
+if(/^rock$/i.test(nb)){
+res=1
+}
+}
+else if(/^scissors$|^s$/i.test(query)){
+query="scissors";
+if(/^paper$/i.test(nb)){
+res=1
+}
+}
+ust=", you chose "+query;
+if (res=1){final=", you win! elizab34Pog"}
+else if (nb===query){", it's a tie! smolYay"}
+else{", you lose! lycelChaos"}
+nbt+ust+final;
+}
