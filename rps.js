9c61f93bc08f53f3aaaba707f488d2cf95e5ee90
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-04-07 22:43:47 -0500

    Update rps.js

--- rps.js
+++ rps.js
@@ -6,39 +6,38 @@ if(query.length==0){
 }
 else{
   res=0;
-  if(/^rock$|^r$/i.test(query)){
+  if(/^rock$/i.test(query)){
     query="rock";
     if(/^scissors$/i.test(nb)){
-      res=1;
+      res=2;
     }
   }
-  else if(/^paper$|^p$/i.test(query)){
+  else if(/^paper$/i.test(query)){
     query="paper";
     if(/^rock$/i.test(nb)){
-      res=1;
+      res=2;
     }
   }
-  else if(/^scissors$|^s$/i.test(query)){
+  else if(/^scissors$/i.test(query)){
     query="scissors";
     if(/^paper$/i.test(nb)){
-      res=1;
+      res=2;
     }
   }
-
-  ust=", you chose "+query;
+  
   nbt="I chose "+nb;
   
-  if (res=1){
+  if (res=2){
     end=", you win! elizab34Pog";
   }
   
-  else if (nb===query){
+  else if (nb=query){
     end=", it's a tie! smolYay";
   }
   
   else{
     end=", you lose! lycelChaos";
   }
-  final=ust+nbt+end;
+  final=nbt+end;
 }
 final
