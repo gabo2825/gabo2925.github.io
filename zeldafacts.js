7fb269e1d79fdc19869ba8cd29c5112e1938f970
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-04-14 17:36:51 -0500

    Update zeldafacts.js

--- zeldafacts.js
+++ zeldafacts.js
@@ -74,7 +74,7 @@ else {
   }
   
   if (/^version$|^v$/i.test(query)) {
-      final=" Idk i'm 11 tries in and nothing's working. - April 14 2021 at 17:31 UTC-5. ";
+      final=" Idk i'm 12 tries in and nothing's working. - April 14 2021 at 17:36 UTC-5. ";
   }
   
   if (/^code$/i.test(query)) {
@@ -88,7 +88,7 @@ if (!isNaN(query)) {
   start=" Did you know ";
   
   factnum=(query-1)%facts.length;
-  num=query+": ";
+  num=(factnum+1)+": ";
   fact=facts[factnum];
   
   emote=omg[Math.floor(Math.random()*omg.length)];
