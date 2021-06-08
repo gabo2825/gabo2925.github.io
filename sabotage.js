747ab1f403f9cab0b93debb7c38f0ac81f66fa5d
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-06-08 17:22:17 -0500

    Create sabotage.js

---
+++ sabotage.js
@@ -0,0 +1,13 @@
+clips=['https://clips.twitch.tv/ReliableDaintyEndiveOpieOP-_Bmh3xw6eLfEkH1A',
+       'the light!! pcrowNo https://clips.twitch.tv/TenderEndearingAardvarkPanicVis-DeKWyIgL_tA5tmBM',
+       'big millie fan. smolYep https://clips.twitch.tv/CrowdedFilthyPlumFunRun-QF05ChDXsVN3ZGZG',
+       'https://clips.twitch.tv/TrustworthyAgreeableChoughFutureMan-Zyg0o9PFwEMu5eVL'];
+
+c=['chair','light','leg','tail'];
+i=c.indexOf(q);
+if(i!=-1) {
+  clips[i]+" lycelYes";
+}
+else {
+  clips[Math.floor(Math.random()*clips.length)];
+}
