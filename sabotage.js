948691a207834fc47ab239348bd94c67fc8ef14b
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-06-08 18:06:48 -0500

    Update sabotage.js

--- sabotage.js
+++ sabotage.js
@@ -1,9 +1,10 @@
 clips=['https://clips.twitch.tv/ReliableDaintyEndiveOpieOP-_Bmh3xw6eLfEkH1A',
        'the light!! pcrowNo https://clips.twitch.tv/TenderEndearingAardvarkPanicVis-DeKWyIgL_tA5tmBM',
        'big millie fan. smolYep https://clips.twitch.tv/CrowdedFilthyPlumFunRun-QF05ChDXsVN3ZGZG',
-       'Tail. lycelGib https://clips.twitch.tv/TrustworthyAgreeableChoughFutureMan-Zyg0o9PFwEMu5eVL'];
+       'Tail. lycelGib https://clips.twitch.tv/TrustworthyAgreeableChoughFutureMan-Zyg0o9PFwEMu5eVL',
+       "can't deal with her sh*t pcrowRip https://clips.twitch.tv/InspiringIntelligentRadishTBTacoRight-0p9PA2xhD_ccaRlB"];
 
-c=['chair','light','leg','tail'];
+c=['chair','light','leg','tail','stinky'];
 i=c.indexOf(q);
 if(i!=-1) {
   clips[i]+" lycelYes";
