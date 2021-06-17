4ca64c379121fc490c549b94440e47bb37c0a442
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-06-17 18:18:41 -0500

    Update sabotage.js

--- sabotage.js
+++ sabotage.js
@@ -1,13 +1,14 @@
-clips=['Try chair, chair2, light, leg, tail, stinky, split.',
+clips=['Try chair, chair2, light, leg, tail, stinky, split, reset.',
        'scratch scratch https://clips.twitch.tv/ReliableDaintyEndiveOpieOP-_Bmh3xw6eLfEkH1A',
-       'back again elizab34Smh ppOverheat https://clips.twitch.tv/AthleticEphemeralToothSmoocherZ-IIpySMv74_thSJSi'
+       'back again elizab34Smh ppOverheat https://clips.twitch.tv/AthleticEphemeralToothSmoocherZ-IIpySMv74_thSJSi',
        'the light!! pcrowNo https://clips.twitch.tv/TenderEndearingAardvarkPanicVis-DeKWyIgL_tA5tmBM',
        'big millie fan. smolYep https://clips.twitch.tv/CrowdedFilthyPlumFunRun-QF05ChDXsVN3ZGZG',
        'Tail. lycelGib https://clips.twitch.tv/TrustworthyAgreeableChoughFutureMan-Zyg0o9PFwEMu5eVL',
        "can't deal with her sh*t pcrowRip https://clips.twitch.tv/InspiringIntelligentRadishTBTacoRight-0p9PA2xhD_ccaRlB",
-       'TRUE SABOTAGE https://clips.twitch.tv/SparklingKathishCiderDansGame-4iLBBnoY8U3G2erk'];
+       'TRUE SABOTAGE https://clips.twitch.tv/SparklingKathishCiderDansGame-4iLBBnoY8U3G2erk',
+       'reset peepD https://clips.twitch.tv/LazySteamyGerbilWutFace-Y_12hvrllNzR2VbO'];
 
-c=['terms','chair','chair2','light','leg','tail','stinky','split'];
+c=['terms','chair','chair2','light','leg','tail','stinky','split','reset'];
 i=c.indexOf(q);
 if(i!=-1) {
   clips[i]+" lycelYes";
