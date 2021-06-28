e062f8daaa0a22318e43b4e5a9d3a223c6e4d982
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-06-27 19:02:56 -0500

    Update sabotage.js

--- sabotage.js
+++ sabotage.js
@@ -2,7 +2,7 @@ if (Math.random() <= 1/100) {
     "Gabo has sabotaged this command smolEvil lycelChaos";
 }
 else {
-    clips=['Try chair, chair2, light, leg, tail, stinky, split, reset.',
+    clips=['Try chair, chair2, light, leg, tail, stinky, split, reset, love.',
            'scratch scratch https://clips.twitch.tv/ReliableDaintyEndiveOpieOP-_Bmh3xw6eLfEkH1A',
            'back again elizab34Smh ppOverheat https://clips.twitch.tv/AthleticEphemeralToothSmoocherZ-IIpySMv74_thSJSi',
            'the light!! pcrowNo https://clips.twitch.tv/TenderEndearingAardvarkPanicVis-DeKWyIgL_tA5tmBM',
@@ -10,9 +10,10 @@ else {
            'Tail. lycelGib https://clips.twitch.tv/TrustworthyAgreeableChoughFutureMan-Zyg0o9PFwEMu5eVL',
            "can't deal with her sh*t pcrowRip https://clips.twitch.tv/InspiringIntelligentRadishTBTacoRight-0p9PA2xhD_ccaRlB",
            'TRUE SABOTAGE https://clips.twitch.tv/SparklingKathishCiderDansGame-4iLBBnoY8U3G2erk',
-           'reset peepD https://clips.twitch.tv/LazySteamyGerbilWutFace-Y_12hvrllNzR2VbO'];
+           'reset peepD https://clips.twitch.tv/LazySteamyGerbilWutFace-Y_12hvrllNzR2VbO',
+           'she loves me so much SWOONER https://www.twitch.tv/kaestrain/clip/LuckyVastEagleCoolStoryBob-AlbEWfmfhHQ8Pqq3?filter=clips&range=30d&sort=time'];
 
-    c=['terms','chair','chair2','light','leg','tail','stinky','split','reset'];
+    c=['terms','chair','chair2','light','leg','tail','stinky','split','reset','love'];
     i=c.indexOf(q);
     if(i!=-1) {
       clips[i]+" lycelYes";
