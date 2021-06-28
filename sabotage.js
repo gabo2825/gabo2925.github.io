e3b4bc2e697ad9add363643c6a6214098823a115
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-06-28 08:24:28 -0500

    Update sabotage.js

--- sabotage.js
+++ sabotage.js
@@ -2,7 +2,7 @@ if (Math.random() <= 1/100) {
     "Gabo has sabotaged this command smolEvil lycelChaos";
 }
 else {
-    clips=['Try chair, chair2, light, leg, tail, stinky, split, reset, SWOONER.',
+    clips=['Try chair, chair2, light, leg, tail, stinky, split, reset, SWOONER, run.',
            'scratch scratch https://clips.twitch.tv/ReliableDaintyEndiveOpieOP-_Bmh3xw6eLfEkH1A',
            'back again elizab34Smh ppOverheat https://clips.twitch.tv/AthleticEphemeralToothSmoocherZ-IIpySMv74_thSJSi',
            'the light!! pcrowNo https://clips.twitch.tv/TenderEndearingAardvarkPanicVis-DeKWyIgL_tA5tmBM',
@@ -11,9 +11,10 @@ else {
            "can't deal with her sh*t pcrowRip https://clips.twitch.tv/InspiringIntelligentRadishTBTacoRight-0p9PA2xhD_ccaRlB",
            'TRUE SABOTAGE https://clips.twitch.tv/SparklingKathishCiderDansGame-4iLBBnoY8U3G2erk',
            'reset peepD https://clips.twitch.tv/LazySteamyGerbilWutFace-Y_12hvrllNzR2VbO',
-           'she loves me so much SWOONER https://clips.twitch.tv/LuckyVastEagleCoolStoryBob-AlbEWfmfhHQ8Pqq3'];
+           'she loves me so much SWOONER https://clips.twitch.tv/LuckyVastEagleCoolStoryBob-AlbEWfmfhHQ8Pqq3',
+           'Fitz noo pcrowPanic https://clips.twitch.tv/FrigidPolishedHyenaPrimeMe-CZGbLlzOU5HSrr5y'];
 
-    c=['terms','chair','chair2','light','leg','tail','stinky','split','reset','swooner'];
+    c=['terms','chair','chair2','light','leg','tail','stinky','split','reset','swooner','run'];
     i=c.indexOf(q);
     if(i!=-1) {
       clips[i]+" lycelYes";
