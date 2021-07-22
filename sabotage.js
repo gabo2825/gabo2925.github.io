3792e2372d898fba95dee30c7a1a73ef4baa1883
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-07-22 01:56:10 -0500

    Update sabotage.js

--- sabotage.js
+++ sabotage.js
@@ -2,7 +2,7 @@ if (Math.random() <= 1/100) {
     "Gabo has sabotaged this command smolEvil lycelChaos";
 }
 else {
-    clips=['Try chair, chair2, light, leg, tail, stinky, split, reset, SWOONER, run.',
+    clips=['Try chair, chair2, light, leg, tail, stinky, split, reset, SWOONER, run, zoom, zoom2.',
            'scratch scratch https://clips.twitch.tv/ReliableDaintyEndiveOpieOP-_Bmh3xw6eLfEkH1A',
            'back again elizab34Smh ppOverheat https://clips.twitch.tv/AthleticEphemeralToothSmoocherZ-IIpySMv74_thSJSi',
            'the light!! pcrowNo https://clips.twitch.tv/TenderEndearingAardvarkPanicVis-DeKWyIgL_tA5tmBM',
@@ -12,9 +12,12 @@ else {
            'TRUE SABOTAGE https://clips.twitch.tv/SparklingKathishCiderDansGame-4iLBBnoY8U3G2erk',
            'reset peepD https://clips.twitch.tv/LazySteamyGerbilWutFace-Y_12hvrllNzR2VbO',
            'she loves me so much SWOONER https://clips.twitch.tv/LuckyVastEagleCoolStoryBob-AlbEWfmfhHQ8Pqq3',
-           'Fitz noo pcrowPanic https://clips.twitch.tv/FrigidPolishedHyenaPrimeMe-CZGbLlzOU5HSrr5y'];
+           'Fitz noo pcrowPanic https://clips.twitch.tv/FrigidPolishedHyenaPrimeMe-CZGbLlzOU5HSrr5y',
+           'smolZoom https://clips.twitch.tv/InquisitiveHorribleMilkRlyTho-aKf_U8BKHW59_vVH',
+           'WoweeSpeed https://clips.twitch.tv/PlumpOnerousCattleAMPEnergy-PkdhV8nxJnel_JBx',
+           'CAUGHT IN 4K! millieJudge https://clips.twitch.tv/KathishAmorphousCroissantUWot-8B9uPfCS3DCnkSXN'];
 
-    c=['terms','chair','chair2','light','leg','tail','stinky','split','reset','swooner','run'];
+    c=['terms','chair','chair2','light','leg','tail','stinky','split','reset','swooner','run','zoom','zoom2','attack'];
     i=c.indexOf(q);
     if(i!=-1) {
       clips[i]+" lycelYes";
