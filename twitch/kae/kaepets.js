897ac58f503987f4f491b0a3d06e053f5a1f8b6f
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-08-03 19:14:07 -0500

    Update kaepets.js

--- twitch/kae/kaepets.js
+++ twitch/kae/kaepets.js
@@ -18,16 +18,26 @@ const love=[' elizab34Uwu ',
             ' lycelLove ',
             ' pcrowLove ',
 	    ' lycelGib ',
-            ' matt38Love '];
+            ' matt38Love ',
+	    ' lycelAAA ',
+	    ' SWOONER '];
 
-const cry=[' kaestr1Cry    ',
+const cool=["kaestr1Cool",
+            "smolCool",
+            "abbybaCool",
+            "lycelCool",
+            "pcrowCool",
+            "archit3Cool",
+            "CoolCat",
+            "maizCool",
+            "therea1896Cool"]
+
+const sad=[' kaestr1Cry    ',
            ' elizab34Hands ',
            ' lycelHands    ',
            ' smolHands     ',
-           ' tomshi1Cry    ',
            ' abbybaCry     ',
            ' archit3Cry    ',
-           ' linkusCry     ',
            ' pcrowCry      ',
            ' smolCry       ',];
 
@@ -37,23 +47,17 @@ const omg=[' smolWow     ',
            ' bryce4Eyes  ',
            ' lycelW      ',
            ' lycelAAA    ',
-           ' smolBrain   ',
-           ' pcrowBrain  ',
-           ' cjyaPog     ',
            ' kaestr1Eyes ',
            ' maizEyes    ',
            ' maizBlush   ',
-           ' abibeaLurk  ',
-           ' elizab34Pog '];
+           ' abibeaLurk  '];
 
 const sleep=[' pcrowCozy ',
              ' pcrowZ ',
              ' maizNap ',
-             ' snazjdComfy ',
              ' cjyaComfy ',
              ' lycelComfy ',
              ' abbybaComfy ',
-             ' hammer89Comfy ',
              ' smolNap '];
 
 const nom=[' smolNom ',
@@ -64,8 +68,19 @@ const lul=[' elizab34LUL ',
            ' smolLUL ',
            ' pcrowGiggle '];
 
+const pout=[' smolPout ',
+	    ' pcrowCute '];
+
+const evil=[' pcrowPanic ',
+	    ' smolPanic ',
+	    ' lycelChaos ',
+	    ' lycelStab ',
+	    ' lycelEvil ',
+            ' smolEvil ',
+            ' pcrowEvil '];
+
 if (/^version$|^v$|^ver$/i.test(query)) {
-    " This thing is working. - August 4 2021 at 18:40 UTC-5. ";
+    " Should be completely functional. - August 3 2021 at 19:14 UTC-5. ";
 }
 else if (/^code$/i.test(query)) {
     " Here, check it out! https://gabo2825.github.io/twitch/kae/kaepets.js ";
@@ -82,7 +97,11 @@ else {
             else {
                 count++
             }
-            facts=["{Insert Millie Fact}"];
+            facts=[" She has a bad attitude problem. "+omg[Math.floor(Math.random()*omg.length)],
+		   " She has a seizure disorder, so she has to take medicine twice a day. "+pout[Math.floor(Math.random()*pout.length)],
+		   " She is very VERY soft. "+love[Math.floor(Math.random()*love.length)],
+		   " She really loves humans, but she despises animals. "+omg[Math.floor(Math.random()*omg.length)],
+		   " She doesn't really like hugs, but we still love her. "+love[Math.floor(Math.random()*love.length)]];
             intro+" demon cat lycelChaos : "+facts[Math.floor(Math.random()*facts.length)];
         }
         
@@ -94,20 +113,28 @@ else {
             else {
                 count++
             }
-            facts=["{Insert Fitz Fact}"];
+            facts=[" He is incredibly big. "+omg[Math.floor(Math.random()*omg.length)]+love[Math.floor(Math.random()*love.length)],
+		   " He loves to cuddle. "+love[Math.floor(Math.random()*love.length)],
+		   " He, for some reason, loves sleeping on top of Kae's dresser. "+lul[Math.floor(Math.random()*lul.length)]+sleep[Math.floor(Math.random()*sleep.length)],
+		   " Millie absolutely despises him. "+evil[Math.floor(Math.random()*evil.length)],
+		   " He doesn't like kisses... but we love himb anyway. "+love[Math.floor(Math.random()*love.length)]];
             intro+" big cat widepeepoHappy : "+facts[Math.floor(Math.random()*facts.length)];
         }
         
         else if (/^Nym$|^n$/i.test(query)) {
             count++;
             if (count>=1) {
-                intro=" Nym is the ";
+                intro=" Nym is the smol cat, a ";
             }
             else {
                 count++
             }
-            facts=["{Insert Nym Fact}"];
-            intro+" 3 legged cutie: "+facts[Math.floor(Math.random()*facts.length)];
+            facts=[" She had an accident long ago, so she lost a leg. "+pout[Math.floor(Math.random()*pout.length)],
+		   " Don't let her cuteness fool you, she's the devil in cat form. "+evil[Math.floor(Math.random()*evil.length)],
+		   " She is small, but she does not care. "+cool[Math.floor(Math.random()*cool.length)],
+		   " Millie could absolutely woop her ass. "+evil[Math.floor(Math.random()*evil.length)],
+		   " She hates a little bit of everything, but we love her anyway. "+love[Math.floor(Math.random()*love.length)]];
+            intro+" 3 legged cutie SWOONER : "+facts[Math.floor(Math.random()*facts.length)];
         }
 	    
 	else if (/^Gwen$|^g$/i.test(query)) {
@@ -118,8 +145,10 @@ else {
             else {
                 count++
             }
-            facts=["{Insert Gwen Fact}"];
-            intro+" Australian Shepherd: "+facts[Math.floor(Math.random()*facts.length)];
+            facts=[" One of her eyes is half brown half blue. "+love[Math.floor(Math.random()*love.length)],
+		   " She tries herding the cats. "+lul[Math.floor(Math.random()*lul.length)],
+		   " She always turn off Kae's computer whenever she lies under the desk. "+omg[Math.floor(Math.random()*omg.length)]];
+            intro+" Australian Shepherd SWOONER : "+facts[Math.floor(Math.random()*facts.length)];
         }
 	    
         else {
