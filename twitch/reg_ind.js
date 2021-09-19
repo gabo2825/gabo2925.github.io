d6f0949f6221eb4ebd048a3028966dd4514e2169
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-09-18 20:17:24 -0500

    Update reg_ind.js

--- twitch/reg_ind.js
+++ twitch/reg_ind.js
@@ -1,3 +1,8 @@
-	$(user) tossed a coin... cjyaBless $(eval q=decodeURIComponent("$(querystring)")+"//";i=0;a=b=n="";while(q[i]!=="/"){if(q[i]===" "&q[i+1]==="/"){}else{a=a+q[i];}i=i+1;}i=i+1;if(q[i]===" "){i=i+1;}while(q[i]!=="/"){if(q[i]===" "&q[i+1]==="/"){}else{b=b+q[i];}i=i+1;}if(b.length!=0){n="(Heads: "+a+". Tails: "+b+".) ";}r=Math.random();if(r <= (99/200)){m="it's Heads! smolYay";}else if(r <= (198/200)){m="it's Tails! smolYay";}else{m="the coin fell on its side! smolD";}n+m;)
+
+
+/previous reference code/
+i=0;
+a=b=n="";
+while(q[i]!=="/"){if(q[i]===" "&q[i+1]==="/"){}else{a=a+q[i];}i=i+1;}i=i+1;if(q[i]===" "){i=i+1;}while(q[i]!=="/"){if(q[i]===" "&q[i+1]==="/"){}else{b=b+q[i];}i=i+1;}if(b.length!=0){n="(Heads: "+a+". Tails: "+b+".) ";}r=Math.random();if(r <= (99/200)){m="it's Heads! smolYay";}else if(r <= (198/200)){m="it's Tails! smolYay";}else{m="the coin fell on its side! smolD";}n+m;)
   
-  If $(user) was a Kae's mod, they'd be $(eval u="$(user)";const m=['N','gabo2825','Gemhunter178','KayOdds','abi_bea','HammerheadLabs','elizabethandcaroline','cynical_en','Mnatthew','Snazjd'];i=m.indexOf(u);if(/jubilantjenna|stardew_rachel/i.test(u)){i=7;}const v =["Ruby","Gabo","Gem","Kay","Abi","Ryan","Caroline","En","Mnatt","Snaz","Nightbot"];if(Math.random()<0.8&&i!=-1){v[i];}else if(u==='xRby1'){'Nightbot';}else{v[Math.floor(Math.random()*v.length)]})!
+If $(user) was a Kae's mod, they'd be $(eval u="$(user)";const m=['N','gabo2825','Gemhunter178','KayOdds','abi_bea','HammerheadLabs','elizabethandcaroline','cynical_en','Mnatthew','Snazjd'];i=m.indexOf(u);if(/jubilantjenna|stardew_rachel/i.test(u)){i=7;}const v =["Ruby","Gabo","Gem","Kay","Abi","Ryan","Caroline","En","Mnatt","Snaz","Nightbot"];if(Math.random()<0.8&&i!=-1){v[i];}else if(u==='xRby1'){'Nightbot';}else{v[Math.floor(Math.random()*v.length)]})!
