712d85900a028fbe0297243a9dc6d73937fd69a9
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-10-24 14:29:21 -0500

    Update sabotage.js

--- twitch/kae/sabotage.js
+++ twitch/kae/sabotage.js
@@ -3,10 +3,16 @@ if (Math.random() <= 1/100) {
     cat[Math.floor(Math.random()*cat.length)]+" has sabotaged this command millieJudge";
 }
 else {
-    c=['terms','chair','chair2','light','leg','tail','stinky','split','reset','swooner','run','zoom','zoom2','attack','attack2','sock','bite','hacker','kiss','growl','surprise','leave'];
-    i=c.indexOf(q);
+    terms=['terms','chair','chair2','light','leg','tail','stinky','split','reset','swooner','run','zoom','zoom2','attack','attack2','sock','bite','hacker','kiss','growl','surprise','leave'];
+    e=terms.indexOf(q);
     
-    clips=['Try chair, chair2, light, leg, tail, stinky, split, reset, swooner, run, zoom, zoom2, attack, attack2. bite, hacker.',
+    i=0;
+    while (i<terms.length) {
+        a=a+terms[i];
+        i++;
+    }
+    
+    clips=['Try '+a+'.',
            'scratch scratch https://clips.twitch.tv/ReliableDaintyEndiveOpieOP-_Bmh3xw6eLfEkH1A',
            'back again elizab34Smh ppOverheat https://clips.twitch.tv/AthleticEphemeralToothSmoocherZ-IIpySMv74_thSJSi',
            'the light!! pcrowNo https://clips.twitch.tv/TenderEndearingAardvarkPanicVis-DeKWyIgL_tA5tmBM',
@@ -30,9 +36,9 @@ else {
            'peepoLeave https://clips.twitch.tv/UnusualFaithfulShallotNotLikeThis-E4fqvSz_7NiBNJvM'];
     
     if(i!=-1) {
-      clips[i]+" lycelYes";
+        clips[e]+" lycelYes";
     }
     else {
-      clips[Math.floor((Math.random()*(clips.length-1))+1)];
+        clips[Math.floor((Math.random()*(clips.length-1))+1)];
     }
 }
