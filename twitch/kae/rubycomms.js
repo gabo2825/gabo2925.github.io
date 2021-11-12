cd44fc78fae4c25f248c21d24a01867fa4683fca
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-11-12 18:44:05 -0500

    Create rubycomms.js

---
+++ twitch/kae/rubycomms.js
@@ -0,0 +1,49 @@
+if (Math.random() <= 1/100) {
+    cat=['Millie','Fitz'];
+    cat["millieJudge "+Math.floor(Math.random()*cat.length)]+" sabotaged this command kaestr1S";
+}
+else {
+    terms=['terms','chair','chair2','light','leg','tail','stinky','split','reset','swooner','run','zoom','zoom2','attack','attack2','sock','bite','hacker','kiss','growl','surprise','leave','raid'];
+    e=terms.indexOf(q);
+    
+    i=1;
+    a='';
+    while (i<terms.length) {
+        a=a+terms[i];
+        if(i<(terms.length-1)){
+            a=a+', ';
+        }
+        i++;
+    }
+    
+    clips=['Try '+a+'.',
+           'scratch scratch https://clips.twitch.tv/ReliableDaintyEndiveOpieOP-_Bmh3xw6eLfEkH1A',
+           'back again elizab34Smh ppOverheat https://clips.twitch.tv/AthleticEphemeralToothSmoocherZ-IIpySMv74_thSJSi',
+           'the light!! pcrowNo https://clips.twitch.tv/TenderEndearingAardvarkPanicVis-DeKWyIgL_tA5tmBM',
+           'big millie fan. smolYep https://clips.twitch.tv/CrowdedFilthyPlumFunRun-QF05ChDXsVN3ZGZG',
+           'Tail. lycelGib https://clips.twitch.tv/TrustworthyAgreeableChoughFutureMan-Zyg0o9PFwEMu5eVL',
+           "can't deal with her sh*t pcrowRip https://clips.twitch.tv/InspiringIntelligentRadishTBTacoRight-0p9PA2xhD_ccaRlB",
+           'TRUE SABOTAGE https://clips.twitch.tv/SparklingKathishCiderDansGame-4iLBBnoY8U3G2erk',
+           'reset peepD https://clips.twitch.tv/LazySteamyGerbilWutFace-Y_12hvrllNzR2VbO',
+           'she loves me so much SWOONER https://clips.twitch.tv/LuckyVastEagleCoolStoryBob-AlbEWfmfhHQ8Pqq3',
+           'Fitz noo pcrowPanic https://clips.twitch.tv/FrigidPolishedHyenaPrimeMe-CZGbLlzOU5HSrr5y',
+           'smolZoom https://clips.twitch.tv/InquisitiveHorribleMilkRlyTho-aKf_U8BKHW59_vVH',
+           'WoweeSpeed https://clips.twitch.tv/PlumpOnerousCattleAMPEnergy-PkdhV8nxJnel_JBx',
+           'CAUGHT IN 4K! millieJudge https://clips.twitch.tv/KathishAmorphousCroissantUWot-8B9uPfCS3DCnkSXN',
+           'MILLIE! https://clips.twitch.tv/SneakyMoralStorkPartyTime-zpXZS4gncWrx0dP8',
+           'HELLO??? https://clips.twitch.tv/QuaintArtsyYamGivePLZ-r7qH3YlkfH-T56fR',
+           'MILLIE STAPH pcrowScream https://clips.twitch.tv/RichFrigidCarrotTooSpicy-zvrJW3LslydJL1Qy',
+           'WoweeHacker https://clips.twitch.tv/GoodAnnoyingOctopusKlappa-DP7qddhSegbvNgoT',
+           'catKISS https://clips.twitch.tv/EsteemedAnnoyingGarageNerfRedBlaster-lZyfT5itWZ7g8n-B',
+           'SHE- smolD millieMad https://clips.twitch.tv/ImpartialTenderNigiriPastaThat-4UU6qAgSkKUIH7Y4',
+           'lycelRage https://clips.twitch.tv/MoistChillyOpossumGOWSkull-G579pcyJAIQgmOUM',
+           'peepoLeave https://clips.twitch.tv/UnusualFaithfulShallotNotLikeThis-E4fqvSz_7NiBNJvM',
+           'she pressed the hate raid button!! kaestr1Madge pcrowPanic thehttps://clips.twitch.tv/HumbleKawaiiCasetteCurseLit-ZRng4fN0ZOH7tSAK'];
+    
+    if(e!=-1) {
+        clips[e]+" lycelYes";
+    }
+    else {
+        clips[Math.floor((Math.random()*(clips.length-1))+1)];
+    }
+}
