302ef82988a832bc4d5d409621e8b6e78e1ce226
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2021-11-12 20:04:54 -0500

    Update rubycomms.js

--- twitch/kae/rubycomms.js
+++ twitch/kae/rubycomms.js
@@ -1,4 +1,4 @@
-if (Math.random() <= 1/100) {
+if (Math.random() <= 1/1000) {
     "pp Peepo ( smolD )";
 }
 else {
@@ -27,27 +27,12 @@ else {
         }
         i++;
     }
-    
-// xrby1 command //
-    xrby1ruby = ['Red guy',
-                 'Frisbee ',
-                 'Uwuby widepeepoHappy',
-                 'W:ohjby',
-                 'Best friends with cicadas',
-                 '6 feet tall at best NODDERS',
-                 'Mamn',
-                 'Roobs',
-                 'Nerd',
-                 'Fitzerby or something idk smolShrug',
-                 'Mod that has been here the longest, for some reason',
-                 'Horse Girl',
-                 'Wtf red guy please eat Smadge'];
-    
-    
-    
+
     comms=['Try '+a+'.',
            "Ruby is cool as a cucumber! He can roast me, but he won't hesitate to abbybaBop if you do - one stink to another stink",
-           ''+xrby1ruby[Math.floor(Math.random()*xrby1ruby.length)],
+           'execute xrby1',
+           gab=1+1;
+           gab;,
            'DAMN IT RUBY this is all your FOOKING fault smolGrump',
            'execute roobify',
            'current query is: "'+query+'".'];
