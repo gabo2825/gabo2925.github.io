230028045c10acb2368124d6902a2947cf744f30
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2022-03-05 21:16:08 -0500

    Update kaepets.js

--- twitch/kae/kaepets.js
+++ twitch/kae/kaepets.js
@@ -83,7 +83,7 @@ const mad=[' millieMad ',
 final="[no message]";
 
 if (/^version$|^v$|^ver$/i.test(query)) {
-	final=" MYAAA but I didn't mess up - March 5 2021 at 21:10 UTC-5. ";
+	final=" MYAAA but I didn't mess up and 'A' is GONE - March 5 2021 at 21:16 UTC-5. ";
 }
 else if (/^code$/i.test(query)) {
 	final=" Here, check it out! https://gabo2825.github.io/twitch/kae/kaepets.js ";
