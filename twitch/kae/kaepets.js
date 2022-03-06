4f28cf402d61fe59d809dddd2db97c901ab127fe
gabo2825 <78673489+gabo2825@users.noreply.github.com>
2022-03-05 21:08:09 -0500

    Update kaepets.js

--- twitch/kae/kaepets.js
+++ twitch/kae/kaepets.js
@@ -83,7 +83,7 @@ const mad=[' millieMad ',
 final="[no message]";
 
 if (/^version$|^v$|^ver$/i.test(query)) {
-    " Added facts for Ms. Kitty and fixed some emotes. - March 5 2021 at 20:40 UTC-5. ";
+    " Added variable 'final' for extra goof stuff - March 5 2021 at 20:40 UTC-5. ";
 }
 else if (/^code$/i.test(query)) {
     " Here, check it out! https://gabo2825.github.io/twitch/kae/kaepets.js ";
@@ -102,14 +102,15 @@ else {
             }
 	    millieemote=['lycelChaos','kaestr1Hug','millieW','millieJudge'];
             facts=[" She has a bad attitude problem. "+omg[Math.floor(Math.random()*omg.length)],
-		   " She has a seizure disorder, so she has to take medicine twice a day. "+pout[Math.floor(Math.random()*pout.length)],
-		   " She is very VERY soft. "+love[Math.floor(Math.random()*love.length)],
-		   " She really loves humans, but she despises animals. "+omg[Math.floor(Math.random()*omg.length)],
-		   " She doesn't really like hugs, but we still love her. "+love[Math.floor(Math.random()*love.length)],
-		   " She zooms like she's possessed. smolZoom "+omg[Math.floor(Math.random()*omg.length)],
-		   " She likes attention, a LOT! "+sleep[Math.floor(Math.random()*sleep.length)],
-		   " She loooves to make biscuits on her blankie "+love[Math.floor(Math.random()*love.length)],
-		   " She is scared of EVERYTHING!! "+omg[Math.floor(Math.random()*omg.length)]];
+								   " She has a seizure disorder, so she has to take medicine twice a day. "+pout[Math.floor(Math.random()*pout.length)],
+		 						   " She is very VERY soft. "+love[Math.floor(Math.random()*love.length)],
+								   " She really loves humans, but she despises animals. "+omg[Math.floor(Math.random()*omg.length)],
+								   " She doesn't really like hugs, but we still love her. "+love[Math.floor(Math.random()*love.length)],
+								   " She zooms like she's possessed. smolZoom "+omg[Math.floor(Math.random()*omg.length)],
+								   " She likes attention, a LOT! "+sleep[Math.floor(Math.random()*sleep.length)],
+								   " She loooves to make biscuits on her blankie "+love[Math.floor(Math.random()*love.length)],
+								   " She is scared of EVERYTHING!! "+omg[Math.floor(Math.random()*omg.length)]];
+					  if(Math.random() <= 1){facts=" MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA ";}
             final=intro+" demon cat "+millieemote[Math.floor(Math.random()*millieemote.length)]+" : "+facts[Math.floor(Math.random()*facts.length)];
         }
         
@@ -128,6 +129,7 @@ else {
 		   " He, for some reason, loves sleeping on top of Kae's dresser. "+lul[Math.floor(Math.random()*lul.length)]+sleep[Math.floor(Math.random()*sleep.length)],
 		   " Millie absolutely despises him. "+evil[Math.floor(Math.random()*evil.length)],
 		   " He doesn't like kisses... but we love himb anyway. "+love[Math.floor(Math.random()*love.length)]];
+					  if(Math.random() <= 1){facts=" MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA ";}
             final=intro+" big cat widepeepoHappy : "+facts[Math.floor(Math.random()*facts.length)];
         }
         
@@ -145,6 +147,7 @@ else {
 		   " She is small, but she does not care. "+cool[Math.floor(Math.random()*cool.length)],
 		   " Millie could absolutely woop her ass. "+evil[Math.floor(Math.random()*evil.length)],
 		   " She hates a little bit of everything, but we love her anyway. "+love[Math.floor(Math.random()*love.length)]];
+						if(Math.random() <= 1){facts=" MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA ";}
             final=intro+" 3 legged cutie SWOONER : "+facts[Math.floor(Math.random()*facts.length)];
         }
 	    
@@ -175,6 +178,7 @@ else {
             facts=[" She MEOWS. MYAAA ",
 		   " She was a stray that adopted Kae's dad "+omg[Math.floor(Math.random()*omg.length)],
 		   " She found Kae's dad while she was pregnant and used him for food "+omg[Math.floor(Math.random()*omg.length)]];
+						if(Math.random() <= 1){facts=" MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA MYAAA ";}
             final=intro+" smol calico cat SWOONER : "+facts[Math.floor(Math.random()*facts.length)];
         }
 	    
@@ -188,5 +192,4 @@ else {
         }
     }   
 }
-
 final;
